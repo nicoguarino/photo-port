a9d45d804407c1399278ad150fd9e460254c73fd
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-12-08 11:15:03 -0800

    finally added without many commits to fix issues with code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact/ContactForm';
 
 function App() {
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -14,6 +16,8 @@ function App() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
@@ -22,11 +26,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
-          <About></About>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
